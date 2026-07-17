f1356f23fd9295f13c328b217b4bd4d12e731e38
V <academwa@gmail.com>
2026-07-17 14:12:59 +0300

    minor fix

--- verification/tb/env/dxi_env.svh
+++ verification/tb/env/dxi_env.svh
@@ -1,30 +1,30 @@
 class dxi_env extends uvm_env;
 
-    function void build_phase();
+    function void build_phase(uvm_phase phase);
         $display("build_phase() %t", $time);
     endfunction
 
-    function void connect_phase();
+    function void connect_phase(uvm_phase phase);
         $display("connect_phase() %t", $time);
     endfunction
 
-    function void end_of_elaboration_phase();
+    function void end_of_elaboration_phase(uvm_phase phase);
         $display("end_of_elaboration_phase() %t", $time);
     endfunction
 
-    function void run_phase();
+    function void run_phase(uvm_phase phase);
         $display("run_phase() %t", $time);
     endfunction
 
-    function void extract_phase();
+    function void extract_phase(uvm_phase phase);
         $display("extract_phase() %t", $time);
     endfunction
 
-    function void check_phase();
+    function void check_phase(uvm_phase phase);
         $display("check_phase() %t", $time);
     endfunction
 
-    function void report_phase();
+    function void report_phase(uvm_phase phase);
         $display("report_phase() %t", $time);
     endfunction
 
